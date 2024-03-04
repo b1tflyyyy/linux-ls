252e5a87c220a79fa970872b3d854947cff209d6
b1tflyyyy <thisisandrew0110@gmail.com>
2024-03-04 14:13:00 -0500

    rewritten parser

--- lib/parser/parser.h
+++ lib/parser/parser.h
@@ -5,4 +5,6 @@
 #include <stdio.h>
 #include <parsing_flags/parsing_flags.h>
 
-enum STATUS Parse_Flags(const char* input, uint8_t* result_expression);
+enum STATUS Parse_Flags(const char* input, uint8_t* result);
+enum STATUS Set_Flag_By_Input_Data(char symbol, uint8_t* result);
+void Set_DMinus_Flag(uint8_t* result, uint8_t flag);
